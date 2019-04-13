59eed82ef05432202be6cd1f2277ee2a70e824da
skovalyova <s.kovalyova@andersenlab.com>
2019-04-13 22:34:20 +0300

    New component and styles

--- src/app/components/tariff-item/tariff-item.component.ts
+++ src/app/components/tariff-item/tariff-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TariffModel } from '../../models/tariff.model';
 
 @Component({
   selector: 'app-tariff-item',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tariff-item.component.scss']
 })
 export class TariffItemComponent implements OnInit {
+  @Input()
+  public tariff: TariffModel;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
--- src/app/components/tariff-list/tariff-list.component.ts
+++ src/app/components/tariff-list/tariff-list.component.ts
@@ -10,14 +10,27 @@ import { TariffModel } from 'src/app/models/tariff.model';
 })
 export class TariffListComponent implements OnInit {
   public tariffs$: Observable<Array<TariffModel>>;
+  public sortableFields: Array<any>;
 
   constructor(private tariffApiService: TariffApiService) { }
 
   ngOnInit() {
     this.tariffs$ = this.tariffApiService.getAll();
+    this.sortableFields = [{
+      fieldName: 'downloadSpeed',
+      displayName: 'Download speed'
+    },
+    {
+      fieldName: 'uploadSpeed',
+      displayName: 'Upload speed'
+    },
+    {
+      fieldName: 'price',
+      displayName: 'Price'
+    }];
   }
 
-  public sortTariffs(fieldName: keyof TariffModel, direction: "ASC" | "DESC") {
+  public sortTariffs(fieldName: keyof TariffModel, direction: "ASC" | "DESC"): void {
     this.tariffs$ = this.tariffApiService.getAll(fieldName, direction);
   }
 }
